993a2b7cdd490b4f816580418775433d5d35f85c
Will Snavely <thoreandan@bilbo.baggins>
2025-09-02 23:38:44 +0000

    Trying to fix nvm

--- wordpress_apache/entry.sh
+++ wordpress_apache/entry.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 if ! [ -e "/srv/www/wordpress/wp-config.php" ]; then
-    python3 /srv/www/gen_php_config.py > /srv/www/wordpress/wp-config.php
+    python3 /app/gen_php_config.py > /srv/www/wordpress/wp-config.php
     chown www-data /srv/www/wordpress/wp-config.php
 fi
 apache2ctl -D FOREGROUND
